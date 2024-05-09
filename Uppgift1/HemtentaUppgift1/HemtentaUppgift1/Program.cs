e05350e2e44917e83145e3f158bf4379a1fb0561
Smalandaren <isakthysell05@gmail.com>
2024-05-09 15:41:51 +0200

    Project start

---
+++ Uppgift1/HemtentaUppgift1/HemtentaUppgift1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HemtentaUppgift1
+{
+	internal static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+
+
+	public class OrderItem
+	{
+		public string soda;
+		public int amount;
+		public bool sugarFree;
+
+		public OrderItem(string Soda, int Amount, bool SugarFree) 
+		{
+			soda = Soda;
+			amount = Amount;
+			sugarFree = SugarFree;
+		}
+
+	}
+
+
+}
